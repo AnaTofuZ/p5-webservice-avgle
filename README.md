e497a3a555e3bf7674ec8eed442e4370d82b9e9b
AnaTofuZ <anatofuz@gmail.com>
2017-10-08 00:21:58 +0900

    first init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Avgle - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Avgle;
+
+# DESCRIPTION
+
+WebService::Avgle is ...
+
+# LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+AnaTofuZ <anatofuz@gmail.com>
---
+++ lib/WebService/Avgle.pm
@@ -0,0 +1,39 @@
+package WebService::Avgle;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Avgle - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Avgle;
+
+=head1 DESCRIPTION
+
+WebService::Avgle is ...
+
+=head1 LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+AnaTofuZ E<lt>anatofuz@gmail.comE<gt>
+
+=cut
+
