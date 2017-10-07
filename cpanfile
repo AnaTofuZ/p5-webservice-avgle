c3f600bdc98c5b1e348ebd95c865a4302e2e7971
AnaTofuZ <anatofuz@gmail.com>
2017-10-08 00:24:14 +0900

    add  get_categories method

--- cpanfile
+++ cpanfile
@@ -1,4 +1,8 @@
 requires 'perl', '5.008001';
+requires 'Furl';
+requires 'URI';
+requires 'JSON';
+requires 'IO::Socket::SSL';
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
--- lib/WebService/Avgle.pm
+++ lib/WebService/Avgle.pm
@@ -2,9 +2,58 @@ package WebService::Avgle;
 use 5.008001;
 use strict;
 use warnings;
+use utf8;
+
+use Furl;
+use URI;
+use Encode;
+use JSON;
+
+use Carp qw/croak/;
 
 our $VERSION = "0.01";
 
+
+sub new {
+   my ($class, %args) = @_;
+
+   my $agent = delete $args{agent} || "WebServie::Avgle agent $VERSION";
+
+   my $self = {
+       base_uri => "https://api.avgle.com",
+       agent    => Furl->new( agent => "$agent"),
+   };
+
+    return bless $self,$class;
+}
+
+sub get_categories {
+    my ($self, %args) = @_;
+
+    my $uri = URI->new($self->{base_uri});
+    $uri->path("/v1/categories");
+
+    my $res = $self->{agent}->get($uri);
+
+    unless ( $res->is_success ){
+        croak "Don't get Avgle video categories";
+    }
+
+    my $pear_hash_data = {};
+
+    eval{
+       $pear_hash_data = decode_json($res->content);
+    };
+    if($@){
+        croak "Can't decode Avgle video categories";
+    }
+
+    if( $pear_hash_data->{success} ){
+        return $pear_hash_data->{response}->{categories};
+    } else {
+        croak "Can't get Avgle video categories";
+    }
+}
 
 
 1;
@@ -14,11 +63,12 @@ __END__
 
 =head1 NAME
 
-WebService::Avgle - It's new $module
+WebService::Avgle - API Client for Avgle
 
 =head1 SYNOPSIS
 
     use WebService::Avgle;
+    my $avgle = WebService::Avgle->new;
 
 =head1 DESCRIPTION
 
